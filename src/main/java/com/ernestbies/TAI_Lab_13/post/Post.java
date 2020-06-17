d66aa406fb66c80391d959bbdd2b1377b9039702
ernestbies <ernestbies@gmail.com>
2020-06-17 16:39:08 +0200

    Lab 14 tasks

--- src/main/java/com/ernestbies/TAI_Lab_13/post/Post.java
+++ src/main/java/com/ernestbies/TAI_Lab_13/post/Post.java
@@ -1,10 +1,22 @@
 package com.ernestbies.TAI_Lab_13.post;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Post {
-    private long id;
+
+    @Id
+    @GeneratedValue
+    private Long id;
     private String title;
     private String url;
     private String content;
+
+    public Post() {
+
+    }
 
     public Post(long id, String title, String url, String content) {
         this.id = id;
@@ -13,32 +25,32 @@ public class Post {
         this.content = content;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getContent() {
         return content;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public void setContent(String content) {
